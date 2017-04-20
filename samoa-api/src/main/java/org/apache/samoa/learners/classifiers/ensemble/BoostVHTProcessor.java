7063e987021ca8d995e4215c75a872c9aa1e108d
Theodore Vasiloudis <tvas@sics.se>
2017-04-20 17:00:52 +0200

    VHT with buffer, stage 2
    Performance seems to be abysmal.

--- samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/BoostVHTProcessor.java
+++ samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/BoostVHTProcessor.java
@@ -285,6 +285,7 @@ public class BoostVHTProcessor implements Processor {
     private int parallelismHint = 1;
     private int timeOut = Integer.MAX_VALUE;
     private ActiveLearningNode.SplittingOption splittingOption;
+    private int maxBufferSize = 1000;
 
     public Builder(Instances dataset) {
       this.dataset = dataset;
@@ -345,6 +346,11 @@ public class BoostVHTProcessor implements Processor {
 
     public Builder splittingOption(ActiveLearningNode.SplittingOption splittingOption) {
       this.splittingOption = splittingOption;
+      return this;
+    }
+
+    public Builder maxBufferSize(int maxBufferSize) {
+      this.maxBufferSize= maxBufferSize;
       return this;
     }
 
